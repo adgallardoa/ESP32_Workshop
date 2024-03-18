ae1af269fbcb4f99a3c8497a92ada71b19a6b47e
Adrian <adgallardoa@gmail.com>
2024-03-18 10:45:08 +0100

    Create DS18B20_Test.ino

---
+++ DS18B20_Test/DS18B20_Test.ino
@@ -0,0 +1,48 @@
+/*
+ * DS18B20 / 1-Wire digital temperature sensor Test!
+ * 
+ * This scketch show how to get temperature data from a DS18B20 sensor connected to the microcontroller.
+ * 
+ * You can use and modify this program under your own responsability
+ * 
+ * by Adrian Gallardo, Cristian Orrego
+ * CEAZA
+ * 
+ * May 2024
+ */
+#include <OneWire.h>              // Communication protocol (1-Wire) used by the sensor
+#include <DallasTemperature.h>    // Temperature sensor controller library
+
+const int oneWireBus = 16;     // GPIO where the DS18B20 is connected to
+
+OneWire oneWire(oneWireBus);  // Setup a oneWire instance to communicate with any OneWire devices
+DallasTemperature sensors(&oneWire);  // Pass our oneWire reference to Dallas Temperature sensor 
+
+
+
+void setup() {
+  delay(100);   // to avoid initial random crash on ESP32 microontroller
+  
+  Serial.begin(115200);
+  while(!Serial){}
+  Serial.println("\nDS-18B20 Temperature Sensor Test!");
+  Serial.println("----------------------------------\n");
+
+  // Start the DS18B20 sensor
+  sensors.begin();
+}
+
+
+
+void loop() {
+  sensors.requestTemperatures();  // Get data from sensor
+  
+  float temperatureC = sensors.getTempCByIndex(0);    // put celsius temperature data on temperatureC variable
+  
+  Serial.print(temperatureC);     // Show temperature value by serial
+  Serial.println("*C");
+  
+  delay(5000);                    // wait 5 seg. 
+
+  // and start again the loop...
+}
